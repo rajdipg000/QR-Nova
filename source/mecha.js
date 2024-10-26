eb1d9f8bde53e12b136894229c2117975a9fffe4
Rajdip Ghosh <pradipg000@gmail.com>
2024-10-26 20:30:01 +0530

    update

--- source/mecha.js
+++ source/mecha.js
@@ -3,14 +3,14 @@
         let tempx = 0, tempy = 0;
     
         $(".cursor").css({
-            "transition": "0.05s ease-in-out",
+            "transition": "0.01s ease-in-out",
             top: (y - 12) + "px",
             left: (x - 12) + "px",
             "width":"15px",
             
         });
         $(".cursor-border").css({
-            "transition": "0.1s ease-in-out",
+            "transition": "0.05s ease-in-out",
             top: (y - 20) + "px",
             left: (x - 20) + "px",
         });
@@ -20,14 +20,14 @@
         let tempx = 0, tempy = 0;
     
         $(".cursor").css({
-            "transition": "0.05s ease-in-out",
+            "transition": "0.01s ease-in-out",
             top: (y - 12) + "px",
             left: (x - 12) + "px",
             "width":"30px",
             
         });
         $(".cursor-border").css({
-            "transition": "0.1s ease-in-out",
+            "transition": "0.5s ease-in-out",
             top: (y - 20) + "px",
             left: (x - 20) + "px",
         });
